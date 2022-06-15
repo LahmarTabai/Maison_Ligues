4d3c6fd3592f01ec3d17800c38d461508bfca98b
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2022-06-15 22:16:35 +0200

    profile user

--- src_php/accueil.php/header_acc.inc.php
+++ src_php/accueil.php/header_acc.inc.php
@@ -10,6 +10,7 @@
                 <li><a href="#films">Sports Collectifs</a></li>
                 <li><a href="#programmes">Top Records</a></li>
                 <li><a href="./history.php"><i class="fas fa-shopping-basket"></i> My History</a></li>
+                <li><a href="./src_php/profil_user_php/profil_user.php"><i class="fas fa-shopping-basket"></i> My Profile</a></li>
                 <li><a href="user_logout.php"><i id="terr" class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
                 <?php
                 $prson = $_SESSION['firstname'];
